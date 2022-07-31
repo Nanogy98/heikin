c69c45763c6a63a82bb6caeb2a38d2c0e7a54aed
Kqin <97442619+Kqin96@users.noreply.github.com>
2022-07-31 23:34:55 +0900

    Update and rename index.py to main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+a=input('1つ目')
+A=int(a)
+b=input('2つ目')
+B=int(b)
+c=input('3つ目')
+C=int(c)
+d=input('3つ目')
+D=int(d)
+e=input('4つ目')
+E=int(e)
+heikin=(A+B+C+D+E)/5
+goukei=A+B+C+D+E
+print('5の値の平均' + str(heikin))
+print('5の値の合計' + str(goukei))
